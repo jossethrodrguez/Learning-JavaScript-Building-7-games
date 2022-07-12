65638a56c325482f9bcc7677ecfb9e98e158bccf
Josseth Rodriguez <hfernand0382@gmail.com>
2022-07-12 15:40:40 -0400

    adding functionality to the buttons

--- rockPaperScissors/app.js
+++ rockPaperScissors/app.js
@@ -1,5 +1,14 @@
 // linked to index.html | rock, paper and scissors game | 7/12/2022
 
-const computerChoice = document.getElementById('computer-choice');
-const usercomputerChoice = document.getElementById('user-choice');
-const resultDisplay = document.getElementById('result');
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+let userChoice
+
+const possibleChoices = document.querySelectorAll('button'); // this choice randomly a option of the buttoms
+
+possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+}    
+))
