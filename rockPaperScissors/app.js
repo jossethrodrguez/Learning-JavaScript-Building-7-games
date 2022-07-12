0f330d96250516ddc8ed5617c7a4098eb5ce34bd
Josseth Rodriguez <hfernand0382@gmail.com>
2022-07-12 16:00:25 -0400

    adding the computer's choice function

--- rockPaperScissors/app.js
+++ rockPaperScissors/app.js
@@ -4,11 +4,29 @@ const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 let userChoice
+let computerChoice
 
 const possibleChoices = document.querySelectorAll('button'); // this choice randomly a option of the buttoms
 
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
 }    
 ))
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1
+    console.log(randomNumber)
+
+    if (randomNumber === 1){
+        computerChoice = 'Rock'
+    }
+    if (randomNumber === 2){
+        computerChoice = 'Paper'
+    }
+    if (randomNumber === 1){
+        computerChoice = 'Scissors'
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoice
+}
