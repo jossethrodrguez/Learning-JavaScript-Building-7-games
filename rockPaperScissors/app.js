2de0b9e0db3749d1992c1496f557b6cc17c55ac3
Josseth Rodriguez <hfernand0382@gmail.com>
2022-07-12 15:21:04 -0400

    adding main variables in js document

---
+++ rockPaperScissors/app.js
@@ -0,0 +1,5 @@
+// linked to index.html | rock, paper and scissors game | 7/12/2022
+
+const computerChoice = document.getElementById('computer-choice');
+const usercomputerChoice = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
